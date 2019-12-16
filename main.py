469d4c3dca229c0421766ed0df1ff7a886153843
Deekshith Shetty <shetty.deekshith07@gmail.com>
2019-12-16 21:57:19 +0100

    added unit clause logic

--- main.py
+++ main.py
@@ -1,8 +1,88 @@
 import sys
 
-trail = []
-assigned_variables = []
-unassigned_variables = []
+# dictionary of variables
+trail = {}
+trail_assignment = []
+
+class Variable:
+	def __init__(self, value, is_decision):
+		self.value, self.is_decision = value, is_decision
+
+
+def find_and_update_unit_clauses(formula):
+	is_unit_clause_updated = 0
+	for clause in formula:
+		literal_counter = 0
+		conjunction_result = False
+		possible_unit_literal = 0
+
+		for literal in clause:
+			variable = abs(literal)
+			if variable in trail: 
+				literal_counter += 1
+				conjunction_result = conjunction_result or (trail[variable].value if literal > 0 else not trail[variable].value)
+			else:
+				possible_unit_literal = literal
+
+		if literal_counter == len(clause) - 1 and conjunction_result == False: # found a unit clause:
+				trail[abs(possible_unit_literal)] = Variable(True if possible_unit_literal > 0 else False, True)
+				trail_assignment.append(abs(possible_unit_literal))
+				is_unit_clause_updated = 1
+		elif literal_counter == len(clause) and not conjunction_result:
+			return -1	# unsatisfied clause
+	
+	return is_unit_clause_updated
+
+
+def BCP(formula):
+	is_unit_clause_found = 1
+	# there is a unit clause implying that a variable x must be set to a value v
+	while (is_unit_clause_found == 1):
+		is_unit_clause_found = find_and_update_unit_clauses(formula)
+
+	if is_unit_clause_found == -1: return False # there is an unsatisfied clause
+	
+	return True	
+	
+
+def decide(nvars):
+	# check if all variables are assigned
+	if len(trail) == nvars: return False;
+	# choose an unassigned variable and set a value
+	unassigned_variable = [item for item in variable_list if item not in trail.keys()][0]
+	trail[unassigned_variable] = Variable(False, False)
+	trail_assignment.append(unassigned_variable)
+	return True
+
+
+def backtrack():
+	while True:
+		if len(trail) == 0: 
+			return False
+			
+		x = trail_assignment.pop()	
+		backtrack_variable = trail.pop(x)		
+		if not backtrack_variable.is_decision:
+			trail[x] = Variable(not backtrack_variable.value, True)
+			trail_assignment.append(x)
+			return True
+
+
+def DPLL(nvars, formula):
+	trail.clear()
+
+	if not BCP(formula): # conflict
+		print("unsat")
+		sys.exit(20)
+
+	while True:
+		if not decide(nvars):
+			print("sat")
+			sys.exit(10)
+		while not BCP(formula): # conflict
+			if not backtrack():
+				print("unsat")
+				sys.exit(20)
 
 
 def parse_dimacs(filename):
@@ -20,63 +100,12 @@ def parse_dimacs(filename):
 	return int(nvars), int(nclauses), formula
 
 
-def decide(nvars):
-	if len(assigned_variables) == nvars:
-		return False;
-	x = unassigned_variables.pop()	
-	assigned_variables.append(x)
-	trail.append([x , 0 , False])
-	return True
-
-
-# TODO
-def BCP(formula):
-	return True
-	# for clause in formula:
-	# 	for literal in clause:
-	# 		if abs(int(literal)) not in unassigned_variables: 
-				
-	# while (there is a unit clause implying that a variable x must be set to a value v ):
-	# 	trail.push([x , v , True])
-	# if (there is an unsatisfied clause): return False
-
-
-def backtrack():
-	while True:
-		if len(trail) == 0: 
-			return False
-		backtrackEntry = trail.pop()
-		if not backtrackEntry[2]:
-			trail.append([backtrackEntry[0] , not backtrackEntry[1] , True])
-			return True
-
-def DPLL(nvars, formula):
-	trail.clear()
-
-	# if not BCP(formula): # conflict
-	# 	print("unsat")
-	# 	sys.exit(20)
-
-	while True:
-		if not decide(nvars):
-			print("sat")
-			sys.exit(10)
-		# while not BCP(formula): # conflict
-		if not backtrack():
-			print("unsat")
-			sys.exit(20)
-
 def main():
+	global variable_list
 	nvars, nclauses, formula = parse_dimacs(sys.argv[1])
-	for clause in formula:
-		for literal in clause:
-			if abs(int(literal)) not in unassigned_variables: 
-				unassigned_variables.append(abs(int(literal)))
-	DPLL(nvars, formula)
+	variable_list = list(range(1, nvars + 1))
 
-	print(trail)
-	print(assigned_variables)
-	print(unassigned_variables)
+	DPLL(nvars, formula)
 
 
 if __name__ == '__main__':
--- solve.sh
+++ solve.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-##### C++
-echo "C++"
-./solve-cpp $*
+#timeout 10s python3 main.py $* ulimit -S -v 4194304
 
-##### Java
-echo "Java"
-java Solver $*
-
-##### Python
-echo "Python"
-python3 example.py $*
+for file in ./benchmarks/*; do
+  echo "$(basename "$file")"
+  timeout 10s python3 main.py "$file" ulimit -S -v 4194304
+  echo "--------------------"
+done
